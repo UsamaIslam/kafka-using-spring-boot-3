cc24491f8e4bcc37f37a06faca66ec3829968bb1
Usama Islam <usama.islam@itmaxglobal.com>
2024-02-05 18:58:54 +0500

    Simple Consumer which recieves the messages and print it

--- consumer/src/main/java/com/project/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/project/consumer/ConsumerApplication.java
@@ -1,13 +1,41 @@
 package com.project.consumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.DltHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.retry.annotation.Backoff;
 
 @SpringBootApplication
 public class ConsumerApplication {
 
+	private final Logger logger = LoggerFactory.getLogger(ConsumerApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
+	}
+
+	@RetryableTopic(attempts = "5", backoff = @Backoff(delay = 2_000, maxDelay = 10_000, multiplier = 2))
+	@KafkaListener(id = "fooGroup", topics = "message-topic")
+	public void listen(String in, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+					   @Header(KafkaHeaders.OFFSET) long offset) {
+
+		this.logger.info("Received: {} from {} @ {}", in, topic, offset);
+		if (in.startsWith("fail")) {
+			throw new RuntimeException("failed");
+		}
+	}
+
+	@DltHandler
+	public void listenDlt(String in, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+						  @Header(KafkaHeaders.OFFSET) long offset) {
+
+		this.logger.info("DLT Received: {} from {} @ {}", in, topic, offset);
 	}
 
 }
